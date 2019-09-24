fae4b9493125147dac7c90d5a427d8b136bf7dae
bufgix <ooruc471@yandex.com>
2019-09-24 13:47:28 +0300

    h2 java ödevi eklendi

---
+++ 2_sinif/h2/odevh2/src/odevh2/Odevh2.java
@@ -0,0 +1,47 @@
+package odevh2;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @author bufgix
+ */
+public class Odevh2 {
+
+    /**
+     * @param args komut satırı argumanları
+     */
+    public static void main(String[] args) {
+        /*
+            args bir Array. Bunun bir List'e dönüşmesi gerek.
+            altaki kod Array'i List'e dönüştürür.
+        
+        */
+        List<String> argsList = Arrays.asList(args);
+
+        if (argsList.containsAll(Arrays.asList("a", "d"))) {
+            for (int i = 1; i < 11; i++) {
+                System.out.print(i + " ");
+            }
+            System.out.println("");
+            for (int i = 10; i > 1; i--) {
+                System.out.print(i + " ");
+            }
+        } else if (argsList.contains("a")) {
+            for (int i = 1; i < 11; i++) {
+                System.out.print(i + " ");
+
+            }
+        } else if (argsList.contains("d")) {
+            for (int i = 10; i > 0; i--) {
+                System.out.print(i + " ");
+            }
+        }
+        else {
+            System.err.println("Args yok");
+        }
+
+    }
+
+}
